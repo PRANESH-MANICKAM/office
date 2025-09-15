ee0065eadc441c3b534aedf598c05d7b5b5603e0
praneshappu2001 <praneshappu2001@gmail.com>
2025-09-15 19:47:42 +0000

    issues

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -9,4 +9,8 @@ const meta: Meta<typeof Header> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    children: 'Header',
+  },
+};
